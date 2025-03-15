a467432f67af1a14fe71fec05a73b47f9d00e6c0
Wade Schulz <wade.schulz@gmail.com>
2025-03-15 18:35:53 -0400

    client and docs updates

---
+++ docs/source/conf.py
@@ -0,0 +1,21 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))  # Ensure your package is found
+
+# Enable autodoc
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # Enables Google-style docstrings
+    "sphinx.ext.viewcode",  # Adds links to source code
+    "sphinx_rtd_theme",  # Enables the ReadTheDocs theme
+]
+
+# Set the ReadTheDocs theme
+html_theme = "sphinx_rtd_theme"
+
+# Ensure docstrings are included for all members
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
