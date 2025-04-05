34c4449ec42c6173523bb59a16db8125aedca17d
Wade Schulz <wade.schulz@gmail.com>
2025-04-05 15:12:46 -0400

    remove hash check

--- tests/int_test_docker.py
+++ tests/int_test_docker.py
@@ -13,18 +13,10 @@ class TestDockerImageIntegration(unittest.TestCase):
         self.docker_image = DockerImage()
         self.repo = "alpine"
         self.tag = "3.20"
-        self.archive_hash = "4dd766dea72b9bcf1d36087dcc45ab6c93e1ce56fbadeedde7514cd1f6bbb90e"
         self.output_dir = tempfile.mkdtemp()
 
     def tearDown(self):
         shutil.rmtree(self.output_dir)
-
-    def get_file_sha256(self, file_path):
-        sha256_hash = hashlib.sha256()
-        with open(file_path, "rb") as f:
-            for chunk in iter(lambda: f.read(8192), b""):
-                sha256_hash.update(chunk)
-        return sha256_hash.hexdigest()
 
     def test_image_exists_locally_and_pull(self):
         # Ensure image is not present
@@ -46,7 +38,6 @@ class TestDockerImageIntegration(unittest.TestCase):
         archive_path = self.docker_image.package_image(self.repo, self.tag, self.output_dir)
         self.assertTrue(os.path.exists(archive_path))
         self.assertTrue(archive_path.endswith(f"/{self.repo}.{self.tag}.tar.gz"))
-        self.assertEqual(self.get_file_sha256(archive_path), self.archive_hash)
 
         # Remove image locally
         self.docker_image.client.images.remove(f"{self.repo}:{self.tag}", force=True)
